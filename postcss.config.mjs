c30e6e2604dba82a90d96e6d9b7664749e750b00
Alexander Cyril <sanchez.alexander.cyril@com>
2025-06-21 16:41:50 +0100

    Added autoprefixer

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,20 @@
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+// export default config;
+
+
+
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {}, // ✅ Required for cross-browser support
   },
 };
 
